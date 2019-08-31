34d04b2223d7d11f1961877d1109587fbbfe8b31
Yash Naik <yashnaik47@gmail.com>
2019-09-01 01:39:12 +0530

    Updates

---
+++ precache-manifest.f87972074f9ed4682976a6f1b37bfb79.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98362264de5fd0f8a8028fe3bac9524a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "730ccbb2096e5c677f62",
+    "url": "/portfolio/static/css/main.6b93a70d.chunk.css"
+  },
+  {
+    "revision": "08f7916140138bb53bcf",
+    "url": "/portfolio/static/js/2.157b6e3b.chunk.js"
+  },
+  {
+    "revision": "730ccbb2096e5c677f62",
+    "url": "/portfolio/static/js/main.be5286dc.chunk.js"
+  },
+  {
+    "revision": "4bc79a2d54f74fff90c2",
+    "url": "/portfolio/static/js/runtime~main.f7d4dbe8.js"
+  },
+  {
+    "revision": "8262057119c963741b868e0d8dd68dd3",
+    "url": "/portfolio/static/media/economica-bold.82620571.ttf"
+  },
+  {
+    "revision": "fd99f8e3744a0cf1335282d72cb77f5f",
+    "url": "/portfolio/static/media/email_icon.fd99f8e3.png"
+  },
+  {
+    "revision": "e2a6288611c5c203fdca4761ca408acf",
+    "url": "/portfolio/static/media/github_icon.e2a62886.png"
+  },
+  {
+    "revision": "76a46530b442922b7667efddf00ac7fa",
+    "url": "/portfolio/static/media/profile.76a46530.png"
+  },
+  {
+    "revision": "bf56edc6c7090e92662e37a9b9ed2aea",
+    "url": "/portfolio/static/media/project1.bf56edc6.png"
+  },
+  {
+    "revision": "f7e79c7d61c8e4a45847337bcec527e6",
+    "url": "/portfolio/static/media/project2.f7e79c7d.png"
+  },
+  {
+    "revision": "7bded960f9e6263b998eb4529af6e041",
+    "url": "/portfolio/static/media/project3.7bded960.png"
+  },
+  {
+    "revision": "cf4449c1884598b292630aa9f4007c19",
+    "url": "/portfolio/static/media/roboto-condensed.light.cf4449c1.ttf"
+  }
+]);
